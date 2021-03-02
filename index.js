1e8d26791c0f40e5a09085a5d5f6bb5281ead8e1
lucas da silva costa <45472635+luccasscds@users.noreply.github.com>
2021-03-02 18:47:55 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log('luccass');
+
+//OJETIVOS
+// o site vai ter dois momentos
+// 1º colocar as fotos e legendas antes de enviar por B.D
+// 2º ver as fotos toda editada com legenda e tudo
+// no estilo do [instagram?]
